bb638a2f39e36e509338c964a02a108663a470de
arafat-ahmed-dev <arafatahmedshlbd194@gmail.com>
2024-05-28 13:25:37 +0600

    done

--- script.js
+++ script.js
@@ -17,20 +17,13 @@ const updateanalogClock = () => {
     hHand.style.transform = `rotate(${hoursAngle}deg)`;
     mHand.style.transform = `rotate(${minutesAngle}deg)`;
     sHand.style.transform = `rotate(${secondsAngle}deg)`;
+    // Display AM or PM
+    const amPm = hours >= 12 ? "PM" : "AM";
+    const formattedHours = hours % 12 || 12; // Convert to 12-hour format
+    const digitalTime = `${formattedHours}:${minutes}:${seconds} ${amPm}`;
+    const clockElement = document.querySelector(".digital-time");
+    clockElement.textContent = digitalTime;
   };
   
   updateanalogClock();
   setInterval(updateanalogClock, 1000);
-
-function updateClock() {
-    const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    const seconds = now.getSeconds();
-
-    const clockElement = document.querySelector(".digital-time");
-    clockElement.textContent = `${hours}:${minutes}:${seconds}`;
-}
-
-// Update the clock every second
-setInterval(updateClock, 1000);
